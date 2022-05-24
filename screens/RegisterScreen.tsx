9a48159b32c7d5c3bdc273667b2bb5ccdf003b9d
HiroyukiMakita <hiroyukimakita.pc@gmail.com>
2022-05-24 23:38:19 +0900

    update: RegisterScreen にフォームを追加

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { KeyboardAvoidingView } from 'react-native';
+import { KeyboardAvoidingView, View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 const RegisterScreen = () => {
 
@@ -13,7 +13,44 @@ const RegisterScreen = () => {
                 flex: 1,
             }}
         >
-            {/* フォーム設定場所 */}
+            <Text style={{ fontSize: 20, marginBottom: 20 }}>ユーザ登録画面</Text>
+            <View style={{ marginBottom: 20 }}>
+                <TextInput
+                    style={{
+                        width: 250,
+                        borderWidth: 1,
+                        padding: 5,
+                        borderColor: 'gray',
+                    }}
+                    // 特定の文字を自動で大文字にする属性を無効
+                    autoCapitalize="none"
+                    // 入力時に表示される候補を表示する属性を無効
+                    autoCorrect={false}
+                />
+            </View>
+            <View style={{ marginBottom: 20 }}>
+                <TextInput
+                    style={{
+                        width: 250,
+                        borderWidth: 1,
+                        padding: 5,
+                        borderColor: 'gray',
+                    }}
+                    placeholder="パスワードを入力してください"
+                    // 入力テキストを隠す
+                    secureTextEntry={true}
+                    autoCapitalize="none"
+                />
+            </View>
+            <TouchableOpacity
+                style={{
+                    padding: 10,
+                    backgroundColor: '#88cb7f',
+                    borderRadius: 10,
+                }}
+            >
+                <Text style={{ color: 'white' }}>登録する</Text>
+            </TouchableOpacity>
         </KeyboardAvoidingView>
     );
 };
