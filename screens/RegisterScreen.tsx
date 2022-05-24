0190acb25cbe0b55297c08028210f4146a14eb36
HiroyukiMakita <hiroyukimakita.pc@gmail.com>
2022-05-25 01:11:28 +0900

    update: 新規登録画面にユーザー登録処理を追加、firebase のコンソールで登録を確認

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../lib/firebase';
 
 const RegisterScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(user);
+        } catch (error) {
+            console.log((error as any).message);
+        }
+    };
 
     return (
         // キーボードに重ならない View
@@ -22,6 +35,9 @@ const RegisterScreen = () => {
                         padding: 5,
                         borderColor: 'gray',
                     }}
+                    onChangeText={setEmail}
+                    value={email}
+                    placeholder="メールアドレスを入力してください"
                     // 特定の文字を自動で大文字にする属性を無効
                     autoCapitalize="none"
                     // 入力時に表示される候補を表示する属性を無効
@@ -36,18 +52,22 @@ const RegisterScreen = () => {
                         padding: 5,
                         borderColor: 'gray',
                     }}
+                    onChangeText={setPassword}
+                    value={password}
                     placeholder="パスワードを入力してください"
                     // 入力テキストを隠す
                     secureTextEntry={true}
                     autoCapitalize="none"
                 />
             </View>
+            {/* ビューをタッチ操作に応答させて見せるために不透明度を変化させるラッパー */}
             <TouchableOpacity
                 style={{
                     padding: 10,
                     backgroundColor: '#88cb7f',
                     borderRadius: 10,
                 }}
+                onPress={handleRegister}
             >
                 <Text style={{ color: 'white' }}>登録する</Text>
             </TouchableOpacity>
