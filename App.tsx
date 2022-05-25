c4b65b2cd9d356c84da5be08409f2234e24638e5
HiroyukiMakita <hiroyukimakita.pc@gmail.com>
2022-05-26 03:34:53 +0900

    update: ログイン状態による表示切り替え追加

--- App.tsx
+++ App.tsx
@@ -1,14 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { auth } from './lib/firebase';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './views/screens/HomeScreen';
+import RegisterScreen from './views/screens/RegisterScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './views/screens/LoginScreen';
+
+/** ユーザーの状態を保持 */
+type UserState = User | null;
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <HomeScreen />
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [user, setUser] = useState(null as UserState);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user: UserState) => {
+      setLoading(false);
+      if (user === null) {
+        setUser(null);
+      } else {
+        console.log(user);
+        setUser(user);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          {
+            user === null
+              ? (
+                <>
+                  <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'ログイン' }} />
+                  <Stack.Screen name="Register" component={RegisterScreen} options={{ title: '新規登録' }} />
+                </>
+              )
+              : <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'ホーム' }} />
+          }
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
